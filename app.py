c9a34acda7e50ff6fff98614f68fe72be8aed924
Poornima Gupta <guptapoornima1605@gmail.com>
2026-03-25 17:48:14 +0530

    Add auto-db-initialization for Render

--- app.py
+++ app.py
@@ -150,6 +150,9 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 db.init_app(app) 
+with app.app_context():
+    db.create_all()
+    print("Database tables created successfully!")
 migrate = Migrate(app, db)
 
 app.register_blueprint(profile_bp)
